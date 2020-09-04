4ee880abc97ced59c25d4a2e162cce02053506ec
amooryjubran <omarsalehjubran@gmail.com>
2020-09-04 00:30:53 -0400

    final push

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll(".slide");
 const dots = document.querySelectorAll(".dot");
+const content = document.getElementById("content");
+const button = document.getElementById("show-more");
+const learnMore = document.getElementById("learn");
 
 const init = (n) => {
   slides.forEach((slide, index) => {
@@ -9,9 +12,41 @@ const init = (n) => {
       dot.classList.remove("active");
     });
   });
+
   slides[n].style.display = "block";
+  slides[n].style.animation = "moveInLeft 2s ease-in-out";
+
   dots[n].classList.add("active");
 };
+
+button.addEventListener("click", (n) => {
+  content.classList.toggle("shrink");
+  content.style.animation = "showDetails 2s ease-in-out";
+
+  if (content.className == "shrink") {
+    content.style.display = "block";
+    button.classList.add("tr-left");
+    button.classList.remove("tr-right");
+    learnMore.style.opacity = "0";
+    learnMore.style.display = "none";
+
+    slides.forEach((slide, index) => {
+      slide.classList.add("horizTranslate");
+      slide.classList.remove("translateBack");
+    });
+  } else {
+    content.style.display = "none";
+    button.classList.add("tr-right");
+    button.classList.remove("tr-left");
+    learnMore.style.opacity = "1";
+    learnMore.style.display = "block";
+    slides.forEach((slide, index) => {
+      slide.classList.remove("horizTranslate");
+      slide.classList.add("translateBack");
+    });
+  }
+});
+
 document.addEventListener("DOMContentLoaded", init(currentSlide));
 const next = () => {
   currentSlide >= slides.length - 1 ? (currentSlide = 0) : currentSlide++;
@@ -29,7 +64,7 @@ document.querySelector(".prev").addEventListener("click", prev);
 
 setInterval(() => {
   next();
-}, 5000);
+}, 8000);
 
 dots.forEach((dot, i) => {
   dot.addEventListener("click", () => {
